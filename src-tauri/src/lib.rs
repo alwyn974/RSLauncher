3923f37dcccbe4dcd690b93a8f3e2a5ea4907f17
Alwyn974 <alwyn974@gmail.com>
2026-07-31 21:26:11 +0400

    feat(plugins): add a lot of plugins
    build(deps): add tauri-plugin-fs v2.5.1
    build(deps): add tauri-plugin-dialog v2.7.2
    build(deps): add tauri-plugin-http v2.5.9
    build(deps): add tauri-plugin-log v2.9.0
    build(deps): add tauri-plugin-os v2.3.2
    build(deps): add tauri-plugin-positioner v2.3.3
    build(deps): add tauri-plugin-updater v2.10.1
    build(deps): add tauri-plugin-window-state v2.4.1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,18 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .build(),
+        )
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
